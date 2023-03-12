a6fdc642293ca2d91011fde3ae564ce68d9b8bdc
lucidus115 <minecrafter141542@gmail.com>
2023-03-12 11:11:46 -0500

    Create a basic search bar for the list page

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const { Pool } = require('pg')
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  // ssl: {
+  //   rejectUnauthorized: false
+  // }
 })
 
 const query = async function (sql, params) {
